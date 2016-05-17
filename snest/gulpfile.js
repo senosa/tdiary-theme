83295a427138d44f20f4cd9c4986c6a7350afafa
Sensuke Osawa <senosa@gmail.com>
2016-05-17 19:32:30 +0900

    Add config for node.js, gulp

---
+++ snest/gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('scss', function () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('scss:watch', function () {
+  gulp.watch('./scss/**/*.scss', ['scss']);
+});
